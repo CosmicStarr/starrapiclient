d1d4264e4b30889c4662947987d19c55b5e7869e
CosmicStarr <CosmicStarr@users.noreply.github.com>
2021-06-14 12:44:15 -0500

    End of Section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'starrapiclient';
+export class AppComponent implements OnInit{
+  title = 'StarrClient';
+  users:any;
+  baseUrl = environment.baseUrl;
+
+  constructor(private httpclient:HttpClient) { 
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.httpclient.get(this.baseUrl + 'api/users').subscribe(results =>{
+      this.users = results;
+    },error =>{
+      console.log(error);
+    });
+  }
 }
